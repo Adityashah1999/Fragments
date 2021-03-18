f76fe51fb87926c3260a31b8765bef24cdc6129f
Aditya Shah <nischalshah1999@gmail.com>
2021-03-18 16:31:24 +0545

    Task2 completed.

--- app/src/main/java/com/example/android/fragmentexample/SimpleFragment.java
+++ app/src/main/java/com/example/android/fragmentexample/SimpleFragment.java
@@ -1,65 +1,61 @@
 package com.example.android.fragmentexample;
 
-import android.app.Fragment;
-import android.os.Build;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.RequiresApi;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RadioGroup;
-import android.widget.RatingBar;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class SimpleFragment extends Fragment {
+
+    // The radio button choice has 3 states: 0 = yes, 1 = no,
+    // 2 = default (no choice). Using only 0 and 1.
     private static final int YES = 0;
     private static final int NO = 1;
 
-    // used for when android needs to re-instantiate a fragment when needed
     public SimpleFragment() {
         // Required empty public constructor
     }
 
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        // setup of root view (layout) and radio group
-        final View rootView = inflater.inflate(R.layout.fragment_simple, container, false);
-        final RadioGroup group = rootView.findViewById(R.id.radio_group);
-        final RatingBar ratingBar = rootView.findViewById(R.id.ratingBar);
+        // Inflate the layout for this fragment.
+        final View rootView = inflater.inflate(R.layout.fragment_simple,
+                container, false);
+        final RadioGroup radioGroup = rootView.findViewById(R.id.radio_group);
 
-        // check change listener for radio group
-        group.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup group, int checkedId) {
-                View radioButton = group.findViewById(checkedId);
-                int index = group.indexOfChild(radioButton);
-                TextView textView = rootView.findViewById(R.id.fragment_header);
-                switch (index) {
-                    case YES:
-                        textView.setText(R.string.yes_message);
-                        break;
-                    case NO:
-                        textView.setText(R.string.no_message);
-                        break;
-                    default: break;
-                }
-            }
-        });
+        // Set the radioGroup onCheckedChanged listener.
+        radioGroup.setOnCheckedChangeListener(
+                new RadioGroup.OnCheckedChangeListener() {
+                    @Override
+                    public void onCheckedChanged(RadioGroup group, int checkedId) {
+                        View radioButton = radioGroup.findViewById(checkedId);
+                        int index = radioGroup.indexOfChild(radioButton);
+                        TextView textView =
+                                rootView.findViewById(R.id.fragment_header);
+                        switch (index) {
+                            case YES: // User chose "Yes".
+                                textView.setText(R.string.yes_message);
+                                break;
+                            case NO: // User chose "No".
+                                textView.setText(R.string.no_message);
+                                break;
+                            default: // No choice made.
+                                // Do nothing.
+                                break;
+                        }
+                    }
+                });
 
-        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
-            @RequiresApi(api = Build.VERSION_CODES.M)
-            @Override
-            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
-                String myRating = (getString(R.string.my_rating) + ratingBar.getRating());
-                Toast.makeText(getContext(), myRating, Toast.LENGTH_SHORT).show();
-            }
-        });
-
+        // Return the View for the fragment's UI.
         return rootView;
     }
 
+    public static SimpleFragment newInstance() {
+        return new SimpleFragment();
+    }
 }
